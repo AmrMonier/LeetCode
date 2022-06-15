7d792fc95889076a9b5b596cf28b2f8d11a2c3ca
Amr Monier <43762965+AmrMonier@users.noreply.github.com>
2022-06-15 15:54:29 +0200

    Time: 7 ms (79.80%), Space: 4.3 MB (27.02%) - LeetHub

--- 1-two-sum/1-two-sum.go
+++ 1-two-sum/1-two-sum.go
@@ -1,12 +1,12 @@
 func twoSum(nums []int, target int) []int {
 
-    hashTable  := map[int]int{}
-    for i, num:= range nums {
-        temp := target- num
-    if j, found := hashTable[temp]; found {
-            return []int{i, j}
-    }
-        hashTable[num] = i
-    }
-    return []int{}
+	hashTable := map[int]int{}
+	for i, num := range nums {
+		temp := target - num
+		if j, found := hashTable[temp]; found {
+			return []int{i, j}
+		}
+		hashTable[num] = i
+	}
+	return []int{}
 }
