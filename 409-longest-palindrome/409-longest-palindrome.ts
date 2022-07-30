8ec29d6568bc530c5017262f8774d79cc93b445c
Amr Monier <43762965+AmrMonier@users.noreply.github.com>
2022-07-30 04:28:56 +0200

    Time: 109 ms (55.76%), Space: 45.7 MB (13.87%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.ts
@@ -0,0 +1,9 @@
+function longestPalindrome(s: string): number {
+    const set = new Set<string>()
+    for(let i = 0; i < s.length; i++){
+        if(set.has(s[i])) set.delete(s[i])
+        else set.add(s[i])
+    }
+    if(set.size <= 1) return s.length
+    return s.length - set.size + 1
+};
