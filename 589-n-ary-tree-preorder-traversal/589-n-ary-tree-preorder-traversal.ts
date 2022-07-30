b8c19d95248dc0ee02bf1c1786410b6bbd26b07a
Amr Monier <43762965+AmrMonier@users.noreply.github.com>
2022-07-31 01:36:11 +0200

    Time: 104 ms (80.07%), Space: 49.2 MB (31.96%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.ts
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.ts
@@ -13,14 +13,12 @@
 
 function preorder(root: Node | null): number[] {
     if(!root) return []
-let childrentQueue = root.children
+let childrentQueue = root.children.reverse()
 const res = [root.val]
 while(childrentQueue.length !== 0){
-    let next = childrentQueue.splice(0,1)
-    res.push(next[0].val)
-    childrentQueue = next[0].children.concat(childrentQueue)
-    
+    let next = childrentQueue.pop()
+    res.push(next.val)
+    childrentQueue = childrentQueue.concat(next.children.reverse())
 }
     return res
-
 };
